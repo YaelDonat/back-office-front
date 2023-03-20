4213ed683860513aebdc2207f17b52b6810170da
AnojanYogeswaran <91149922+AnojanYogeswaran@users.noreply.github.com>
2023-03-20 14:34:57 +0100

    images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <main></main>
+    <main>
+      <div class="randomimage">
+        <img :src="getRandomImage()" alt="Random Image" />
+      </div>
+    </main>
   </div>
 </template>
 
@@ -10,6 +14,23 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'HomeView',
   components: {},
+  data() {
+    return {
+      currentImageKey: null,
+    }
+  },
+  methods: {
+    getRandomImage() {
+      const images = require.context('../assets/Home-pictures', false)
+      const keys = images.keys()
+      let randomKey = keys[Math.floor(Math.random() * keys.length)]
+      while (randomKey === this.currentImageKey) {
+        randomKey = keys[Math.floor(Math.random() * keys.length)]
+      }
+      this.currentImageKey = randomKey
+      return images(randomKey)
+    },
+  },
 })
 </script>
 
@@ -27,5 +48,14 @@ main {
   justify-content: center;
   flex-direction: column;
   overflow-y: hidden;
+}
+
+.randomimage {
+  padding-top: 2%;
+  background-size: cover;
+  height: auto;
+  max-width: 100%;
+  background: center;
+  background-size: cover 50% 50%;
 }
 </style>
